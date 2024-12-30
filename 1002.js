c6aa2daca197dfb9df9a1d490f51f4dbc7daf88f
Nadia Islam <nadiaislam1099@gmail.com>
2024-12-30 13:25:05 -0800

    1002.js

--- 1002.js
+++ 1002.js
@@ -1,5 +1,5 @@
 //The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
-const R = parseFloat(require('fs').readFileSync('/dev/stdin', 'utf8'));
+const R = parseFloat(require('fs').readFileSync('/dev/stdin', 'utf8')); //read the float value of R 
 const π = 3.14159; 
 const A = π * R * R; 
-console.log("A=" + A.toFixed(4));
+console.log("A=" + A.toFixed(4));//Here toFixed(4) i used for taking four places after the decimal point
